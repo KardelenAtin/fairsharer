e4b0cbeffae2787573b14c824046a04009401d86
Kardelen Atin <kardelen.atin@study.hs-duesseldorf.de>
2026-01-11 01:53:38 +0100

    copied the general code

--- fairsharer/my_module.py
+++ fairsharer/my_module.py
@@ -1,29 +1,19 @@
-"""Documentation about the fairsharer module."""
-
-
-# FIXME: put actual code here
-def hello(name):
-    """Say hello.
-
-    Function docstring using Google docstring style.
-
+def fair_sharer(values, num_iterations, share=0.1):
+    """Runs num_iterations.
+    In each iteration the highest value in "values" gives a fraction (share)
+    to both the left and right neighbor. The leftmost field is considered
+    the neighbor of the rightmost field.
+    
+    Examples:
+    fair_sharer([0, 1000, 800, 0], 1) --> [100, 800, 900, 0]
+    fair_sharer([0, 1000, 800, 0], 2) --> [100, 890, 720, 90]
+    
     Args:
-        name (str): Name to say hello to
-
-    Returns:
-        str: Hello message
-
-    Raises:
-        ValueError: If `name` is equal to `nobody`
-
-    Example:
-        This function can be called with `Jane Smith` as argument using
-
-        >>> from fairsharer.my_module import hello
-        >>> hello('Jane Smith')
-        'Hello Jane Smith!'
-
+    values: 
+        1D array of values (list or numpy array)
+    num_iterations: Integer to set the number of iterations
+    share: Fraction of the value to be shared (default 0.1)
     """
-    if name == "nobody":
-        raise ValueError("Cannot say hello to nobody")
-    return f"Hello {name}!"
+values_new = list(values)
+n = len(values_new)
+#return values_new
