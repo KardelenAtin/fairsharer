6bc5867604e3b45c2184d4c31f23ab0e8fc11e0f
Kardelen Atin <kardelen.atin@study.hs-duesseldorf.de>
2026-01-11 21:34:26 +0100

    Fix import formatting for ruff

--- tests/test_fairsharer.py
+++ tests/test_fairsharer.py
@@ -1,9 +1,12 @@
 """Tests for the fairsharer"""
 
-
 from fairsharer.fairsharer import fair_sharer
+
 
 def test_fair_sharer():
     assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0]
     assert fair_sharer([0, 1000, 800, 0], 2) == [100, 890, 720, 90]
     assert fair_sharer([0, 0, 0, 10], 1) == [1, 0, 1, 8]
+
+
+
